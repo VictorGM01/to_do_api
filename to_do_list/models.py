c9cd3a0b0314a05d01bb5ffb07a5ee727adfaaa9
Victor G. Marques <victormarques8801@gmail.com>
2022-12-24 00:32:52 -0300

    feat: cria modelo para as tarefas

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    data = models.DateField(default=datetime.now(), blank=True)
+    status = models.BooleanField(default=False)
+    descricao = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.titulo
