7b43e06b80dcb3248e604aa8c6099bcd77468ec1
Victor G. Marques <victormarques8801@gmail.com>
2022-12-24 00:39:09 -0300

    feat: registra modelo no painel administrativo

--- to_do_list/admin.py
+++ to_do_list/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tarefa
 
-# Register your models here.
+
+class Tarefas(admin.ModelAdmin):
+    list_display = ("id", "titulo", "data", "status")
+    list_display_links = ("id", "titulo")
+    search_fields = ("titulo", )
+    list_editable = ('status', )
+    list_filter = ('status',)
+    list_per_page = 10
+
+
+admin.site.register(Tarefa, Tarefas)
