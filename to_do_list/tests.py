7c3a279e28d521f712d802b93c39e1a1257c03e5
Victor G. Marques <victormarques8801@gmail.com>
2022-12-26 16:23:05 -0300

    test: verifica se o status code do get('/') é 200

--- to_do_list/tests.py
+++ to_do_list/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from to_do_list.models import Tarefa
+from datetime import datetime
 
-# Create your tests here.
+
+class TestTarefas(APITestCase):
+
+    def test_deve_retornar_status_code_200_no_metodo_get(self):
+        resposta = self.client.get("/")
+        self.assertEqual(resposta.status_code, status.HTTP_200_OK)
