b9dafbf183f5063e13f4513c6d67f9adc8e59d1b
Victor G. Marques <victormarques8801@gmail.com>
2022-12-26 19:33:40 -0300

    fix: corrige valor default do campo de data
    Implementação correta do commit #71c72d1 do PR #10

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
-    data_de_conclusao = models.DateField(default=timezone.now(), blank=True, null=True)
+    data_de_conclusao = models.DateField(default=timezone.now, blank=True, null=True)
     concluida = models.BooleanField(default=False)
     descricao = models.TextField(max_length=500, blank=True)
 
