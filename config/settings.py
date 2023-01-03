52c34f3247f1f4ab4e56f5626ee83ede42bf3e4c
Victor G. Marques <victormarques8801@gmail.com>
2023-01-03 19:10:03 -0300

    feat: adiciona variáveis de ambiente com dotenv

--- config/settings.py
+++ config/settings.py
@@ -1,19 +1,22 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!gdiw@j^=nguf1ps4g@03l$ta!g6uea(@*ufjj9b6tnhxdoex'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
