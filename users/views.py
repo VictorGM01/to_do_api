a6337cff157ac4d0766f0bc26e7be7bdfd51e170
Victor G. Marques <victormarques8801@gmail.com>
2023-01-09 13:23:28 -0300

    feat: permite cadastro no sistema via POST

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def cadastro(request):
+    """Cadastra um novo usuário no sistema"""
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if User.objects.filter(email=email).exists():
+            return Response({'status': 'e-mail já cadastrado'}, status=status.HTTP_404_NOT_FOUND)
+
+        elif User.objects.filter(username=nome).exists():
+            return Response({'status': 'username já utilizado'}, status=status.HTTP_404_NOT_FOUND)
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+
+        return Response({'status': 'criado'}, status=status.HTTP_201_CREATED)
