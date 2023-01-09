adb210b08d0c659c4570257b9858957e45371aa6
Victor G. Marques <victormarques8801@gmail.com>
2023-01-09 16:16:21 -0300

    feat: adiciona novo campo no modelo de Tarefa
    Adição da ForeignKey, de modo que este campo esteja vinculado à tabela de usuários

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import date
+from django.contrib.auth.models import User
 
 
 class Tarefa(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
     data_de_conclusao = models.DateField(default=date.today, blank=True, null=True)
     concluida = models.BooleanField(default=False)
