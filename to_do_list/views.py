f6d5c7a5fa85fa22e7bd1847a985c47f44a32b57
Victor G. Marques <victormarques8801@gmail.com>
2023-01-09 16:09:31 -0300

    feat: adiciona obrigatoriedade da autenticação

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from to_do_list.models import Tarefa
 from to_do_list.serializer import TarefaSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class TarefaViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
